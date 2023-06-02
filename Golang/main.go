de471f33edec03f0c79d7f5fbfc2ba61428124a5
肥羊 <youshandefeiyang@gmail.com>
2023-06-02 11:00:59 +0800

    增加huya、douyu的m3u列表实时获取

--- Golang/main.go
+++ Golang/main.go
@@ -8,11 +8,15 @@
 package main
 
 import (
+	"Golang/list"
 	"Golang/liveurls"
 	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"github.com/forgoer/openssl"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func duanyan(adurl string, realurl any) string {
@@ -34,6 +38,65 @@ func setupRouter(adurl string) *gin.Engine {
 		douyinobj := &liveurls.Douyin{}
 		douyinobj.Shorturl = url
 		c.Redirect(http.StatusMovedPermanently, duanyan(adurl, douyinobj.GetRealurl()))
+	})
+
+	r.GET("/huyayqk.m3u", func(c *gin.Context) {
+		yaobj := &list.HuyaYqk{}
+		res, _ := yaobj.HuYaYqk("https://live.cdn.huya.com/liveHttpUI/getLiveList?iGid=2135")
+		var result list.YaResponse
+		json.Unmarshal(res, &result)
+		pageCount := result.ITotalPage
+		pageSize := result.IPageSize
+		c.Writer.Header().Set("Content-Type", "application/octet-stream")
+		c.Writer.Header().Set("Content-Disposition", "attachment; filename=HuYaYqk.m3u")
+
+		fmt.Fprintln(c.Writer, "#EXTM3U")
+		fmt.Fprintln(c.Writer, "#EXTINF:-1 tvg-name=\"4K60PSDR-H264-AAC测试\" tvg-logo=\"https://cdn.jsdelivr.net/gh/youshandefeiyang/IPTV/logo/tg.jpg\" group-title=\"4K频道\",4K60PSDR-H264-AAC测试")
+		fmt.Fprintln(c.Writer, "http://159.75.85.63:5680/d/ad/h264/playad.m3u8")
+		fmt.Fprintln(c.Writer, "#EXTINF:-1 tvg-name=\"4K60PHLG-HEVC-EAC3测试\" tvg-logo=\"https://cdn.jsdelivr.net/gh/youshandefeiyang/IPTV/logo/tg.jpg\" group-title=\"4K频道\",4K60PHLG-HEVC-EAC3测试")
+		fmt.Fprintln(c.Writer, "http://159.75.85.63:5680/d/ad/playad.m3u8")
+
+		for i := 1; i <= pageCount; i++ {
+			apiRes, _ := yaobj.HuYaYqk(fmt.Sprintf("https://live.cdn.huya.com/liveHttpUI/getLiveList?iGid=2135&iPageNo=%d&iPageSize=%d", i, pageSize))
+			var res list.YaResponse
+			json.Unmarshal(apiRes, &res)
+			data := res.VList
+			for _, value := range data {
+				fmt.Fprintf(c.Writer, "#EXTINF:-1 tvg-logo=\"%s\" group-title=\"%s\", %s\n", value.SAvatar180, value.SGameFullName, value.SNick)
+				fmt.Fprintf(c.Writer, "https://www.goodiptv.club/huya/%v\n", value.LProfileRoom)
+			}
+		}
+	})
+
+	r.GET("/douyuyqk.m3u", func(c *gin.Context) {
+		yuobj := &list.DouYuYqk{}
+		resAPI, _ := yuobj.Douyuyqk("https://www.douyu.com/gapi/rkc/directory/mixList/2_208/list")
+
+		var result list.DouYuResponse
+		json.Unmarshal(resAPI, &result)
+		pageCount := result.Data.Pgcnt
+
+		c.Writer.Header().Set("Content-Type", "application/octet-stream")
+		c.Writer.Header().Set("Content-Disposition", "attachment; filename=Douyuyqk.m3u")
+
+		fmt.Fprintln(c.Writer, "#EXTM3U")
+		fmt.Fprintln(c.Writer, "#EXTINF:-1 tvg-name=\"4K60PSDR-H264-AAC测试\" tvg-logo=\"https://cdn.jsdelivr.net/gh/youshandefeiyang/IPTV/logo/tg.jpg\" group-title=\"4K频道\",4K60PSDR-H264-AAC测试")
+		fmt.Fprintln(c.Writer, "http://159.75.85.63:5680/d/ad/h264/playad.m3u8")
+		fmt.Fprintln(c.Writer, "#EXTINF:-1 tvg-name=\"4K60PHLG-HEVC-EAC3测试\" tvg-logo=\"https://cdn.jsdelivr.net/gh/youshandefeiyang/IPTV/logo/tg.jpg\" group-title=\"4K频道\",4K60PHLG-HEVC-EAC3测试")
+		fmt.Fprintln(c.Writer, "http://159.75.85.63:5680/d/ad/playad.m3u8")
+
+		for i := 1; i <= pageCount; i++ {
+			apiRes, _ := yuobj.Douyuyqk("https://www.douyu.com/gapi/rkc/directory/mixList/2_208/" + strconv.Itoa(i))
+
+			var res list.DouYuResponse
+			json.Unmarshal(apiRes, &res)
+			data := res.Data.Rl
+
+			for _, value := range data {
+				fmt.Fprintf(c.Writer, "#EXTINF:-1 tvg-logo=\"https://apic.douyucdn.cn/upload/%s_big.jpg\" group-title=\"%s\", %s\n", value.Av, value.C2name, value.Nn)
+				fmt.Fprintf(c.Writer, "https://www.goodiptv.club/douyu/%v\n", value.Rid)
+			}
+		}
 	})
 
 	r.GET("/:path/:rid", func(c *gin.Context) {
