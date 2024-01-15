857dde3c70be38410bc265ce351e4abdaa9158f1
肥羊 <youshandefeiyang@gmail.com>
2024-01-15 19:36:45 +0800

    Add files via upload

--- PHP/huya.php
+++ PHP/huya.php
@@ -33,6 +33,9 @@ function getLiveUrl($rid, $cdn, $cdnType, $mediaurl)
     $liveurl = "https://m.huya.com/" . $rid;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $liveurl);
+    curl_setopt($ch, CURLOPT_ENCODING, "gzip");
+    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/604.1",
